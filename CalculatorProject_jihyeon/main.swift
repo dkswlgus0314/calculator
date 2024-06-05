00a40c37a3f64900db110ab62bd73db31fc622e4
jihyeon <dkswlgus0314@naver.com>
2024-06-05 15:27:58 +0900

    1. switch 구문 활용해 해당 연산 조건에 따른 연산 메서드 연결 2. 콘솔창에 입력된 값을 readLine()으로 클래스에 받아오기

--- CalculatorProject_jihyeon/main.swift
+++ CalculatorProject_jihyeon/main.swift
@@ -14,97 +14,86 @@
 
 
 
-
-//임의의 값 할당
-var testInput1 = 2
-var testOper = "*"
-var testInput2 = 5
-
-// 콘솔창에 숫자 입력
-//var inputValue = readLine() //String?
-
-
-//if let userFirstNum = inputValue {     //String
-//    if let num1 = Int(userFirstNum) {
-//        print("테스트 \(num1)")
-//    }
-    
-//    guard let num1 = Int(userFirstNum) else { print("edfsdf") }
-    
-    
-//}
-
 class Calculator {
     
     //속성(property)
     var firstNumber: Int
-    var operatorArray: String
+    var operators: String
     var secondNumber: Int
     
     
     //이니셜라이저
-    init(firstNumber: Int, operatorArray: String, secondNumber: Int) {
+    init(firstNumber: Int, operators: String, secondNumber: Int) {
         self.firstNumber = firstNumber
-        self.operatorArray = operatorArray
+        self.operators = operators
         self.secondNumber = secondNumber
+    }
+    
+    
+    func calculate() -> Double {
+        switch operators {
+        case "+" : return add()
+        case "-" : return subtract()
+        case "*" : return multiply()
+        case "/" : return divide()
+        case "%" : return remainder()
+        default: return 0
+        }
     }
     
     
     //연산 메서드
     //덧셈
     func add() -> Double{
-        return Double(firstNumber) + Double(secondNumber)
+        return Double(firstNumber + secondNumber)
     }
     
     //뺄셈
-    func subtract(firstNumber: Int, secondNumber: Int) -> Double{
-        return Double(firstNumber) - Double(secondNumber)
+    func subtract() -> Double{
+        return Double(firstNumber - secondNumber)
     }
     
     //곱셈
-    func multiply(firstNumber: Int, secondNumber: Int) -> Double{
-        return Double(firstNumber) * Double(secondNumber)
+    func multiply() -> Double{
+        return Double(firstNumber * secondNumber)
     }
     
     //나눗셈
-    func divide(firstNumber: Int, secondNumber: Int) -> Double{
-        return Double(firstNumber) / Double(secondNumber)
+    func divide() -> Double{
+        return Double(firstNumber / secondNumber)
     }
     
     //나머지
-    func remainder(firstNumber: Int, secondNumber: Int) -> Double{
+    func remainder() -> Double{
         return Double(firstNumber % secondNumber)
     }
-    
-    
 }
 
 
+// 콘솔창에 숫자 입력
+while true {
+    guard let inputValue = readLine(), let num1 = Int(inputValue) else {     //Int
+        print("[ 숫자만 입력해주세요. ]")
+        continue
+    }
+    guard let ops = readLine() else {     //String
+        print("[ +, -, /, *, % 중에서만 입력해주세요. ]")
+        continue
+    }
+    guard let inputValue = readLine(), let num2 = Int(inputValue) else {     //Int
+        print("[ 숫자만 입력해주세요 ]")
+        continue
+    }
+    
+    //인스턴스 생성하여 변수에 할당
+    let calculator = Calculator(firstNumber: num1, operators: ops, secondNumber: num2)
 
+    
+    var result = calculator.calculate()
 
-//인스턴스 생성하여 변수에 할당
-//let calculator = Calculator(firstNumber: Int(readLine()!)! , operatorArray: testOper , secondNumber: testInput1)
-
-//let calculator = Calculator()
-
-let calculator = Calculator(firstNumber: Int(readLine()!)! , operatorArray: readLine()! , secondNumber: Int(readLine()!)!)
-
-
-var result = calculator.add()
-
-print(result)
-
-
-
-
-
-
-//let addResult =     //덧셈 연산
-//let subtractResult =  //뺄셈 연산
-//let multiplyResult = //곱셈 연산
-//let divideResult = //나눗셈 연산
-
-
+    print(result)
+    break 
+}
 
 
 
